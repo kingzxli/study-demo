326f9139e6504f5f27f5d700ba3ff11a2731e8d8
king <li.zhengxue@infox.com.cn>
2020-05-31 08:49:30 +0800

    fix

---
+++ study-demo/src/site/db/study.sql
@@ -0,0 +1,39 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : docker_mysql
+ Source Server Type    : MySQL
+ Source Server Version : 50730
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : study
+
+ Target Server Type    : MySQL
+ Target Server Version : 50730
+ File Encoding         : 65001
+
+ Date: 31/05/2020 08:47:43
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept` (
+  `id` varchar(32) NOT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `db_source` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+BEGIN;
+INSERT INTO `dept` VALUES ('1', 'king', '源1');
+INSERT INTO `dept` VALUES ('2', 'fang', '源1');
+COMMIT;
+
+SET FOREIGN_KEY_CHECKS = 1;
